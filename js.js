f7bf1de24899d3b5ef848e7d25b5a42bb9fa14c0
asqool <91128487+asqool@users.noreply.github.com>
2022-09-08 18:18:41 +0200

    Update js.js

--- js.js
+++ js.js
@@ -1,7 +1,4 @@
 
 function onload(){
-  var paragraph = document.getElementById("p");
-  var text = document.createTextNode("This just got added");
-  paragraph.appendChild(text);
-
+  var paragraph = document.getElementById("p").textContent('aaaaaasssssss')
 }
