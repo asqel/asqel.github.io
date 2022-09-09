b9b0c876e610386fec8b63e426627d51292f5706
asqool <91128487+asqool@users.noreply.github.com>
2022-09-09 15:49:37 +0200

    Create js.js

--- js.js
+++ js.js
@@ -2,13 +2,15 @@ var url="https://asqool.github.io/j.json"
 var t=[]
 fetch(url).then(function(res){
   t.push(res.text())
+
 })
 
 
 
 
 function onload(){
-  console.log(t)
+  console.log(JSON.parse(t))
+  console.log(JSON.parse(t.fulfilled))
   document.write("salut toi");
   document.write(t.fulfilled);
 }
