b33e8495b908054e131491a7ac4edb8e65ec6da4
asqool <91128487+asqool@users.noreply.github.com>
2022-09-09 12:23:26 +0200

    Update js.js

--- js.js
+++ js.js
@@ -1,13 +1,14 @@
 var url="https://asqool.github.io/j.json"
 var t=[]
 fetch(url).then(function(res){
-  t.push(res.text);
-});
+  t.push(res.text)
+})
 
 
 
 
 function onload(){
+  console.log(t)
   document.write("salut toi");
-  document.write(t[0]);
+  document.write(t);
 }
