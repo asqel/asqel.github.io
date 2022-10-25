37e3dd3a51a5e2440fc9b9256ffabb2b552196b3
asqool <axel.lleres@gmail.com>
2022-10-25 20:54:25 +0200

    1

--- js.js
+++ js.js
@@ -1,16 +1,3 @@
-var url="https://asqool.github.io/j.json"
-var t=[]
-fetch(url).then(function(res){
-  t.push(res.text())
-
-})
-
-
-function onload(){
-  console.log(JSON.parse(t.fulfilled))
-  document.write("salut toi");
-  document.write(t.fulfilled);
-}
 
 function exec_bf(){
   text=document.getElementById("bf_id").contentEditable;
